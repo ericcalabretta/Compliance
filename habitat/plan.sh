65bd9b5e9a11541cc318376e6dc821b2bdd4a1d7
ericcalabretta <eric.calabretta@gmail.com>
2018-06-21 09:01:32 -0500

    first

---
+++ habitat/plan.sh
@@ -0,0 +1,19 @@
+pkg_name=compliance
+pkg_origin=eric
+pkg_version="0.1.0"
+pkg_maintainer="Eric Calabretta <ecalabretta@chef.io>"
+pkg_license=("Apache-2.0")
+pkg_deps=("chef/inspec" "core/cacerts")
+pkg_svc_user="root"
+
+do_download() {
+  return 0;
+}
+
+do_build() {
+  return 0;
+}
+
+do_install() {
+  return 0;
+}
